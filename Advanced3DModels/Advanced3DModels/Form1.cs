3f7f8b5cf697b98449213592d7270445594bfd25
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-01-13 11:47:22 +0300

    The core of appliaction for sphere is ready.

--- Advanced3DModels/Advanced3DModels/Form1.cs
+++ Advanced3DModels/Advanced3DModels/Form1.cs
@@ -16,5 +16,62 @@ namespace Advanced3DModels
         {
             InitializeComponent();
         }
+
+        #region === members ===
+
+        Bitmap _bitmap;
+
+        #endregion
+
+        #region === private methods ===
+
+        void RenderSphere(Graphics g, int Xc, int Yc, int radius)
+        {
+            // TODO:
+        }
+
+        void Render()
+        {
+            if (_bitmap == null)
+                return;
+
+            Graphics g = Graphics.FromImage(_bitmap);
+            g.Clear(Color.White);
+
+            RenderSphere(g, pictureBox1.Width / 2, pictureBox1.Height / 2, pictureBox1.Width / 2 - pictureBox1.Width / 10);
+
+            pictureBox1.Image = _bitmap;
+        }
+
+        bool CreateBackground()
+        {
+            if (pictureBox1.Width < 1 || pictureBox1.Height < 1)
+            {
+                _bitmap = null;
+                return false;
+            }
+
+            _bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            return true;
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+            CreateBackground();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }

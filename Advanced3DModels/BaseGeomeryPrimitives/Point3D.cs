114412ad60e6af326ac93968ac4812e760f668d1
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-01-14 17:28:54 +0300

    Point3D class has been added.

---
+++ Advanced3DModels/BaseGeomeryPrimitives/Point3D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseGeomeryPrimitives
+{
+    public class Point3D
+    {
+        readonly double[] _X;
+
+        public Point3D(double X, double Y, double Z)
+        {
+            _X = new double[3];
+            _X[0] = X;
+            _X[1] = Y;
+            _X[2] = Z;
+        }
+
+        public double X
+        {
+            get { return _X[0]; }
+            set { _X[0] = value; }
+        }
+
+        public double Y
+        {
+            get { return _X[1]; }
+            set { _X[1] = value; }
+        }
+
+        public double Z
+        {
+            get { return _X[2]; }
+            set { _X[2] = value; }
+        }
+    }
+}

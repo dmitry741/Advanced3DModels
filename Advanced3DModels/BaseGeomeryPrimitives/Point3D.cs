07f21bb06f39baba71824c8cd36454a320a8a876
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-01-15 17:32:34 +0300

    Use System.Numeric

--- Advanced3DModels/BaseGeomeryPrimitives/Point3D.cs
+++ Advanced3DModels/BaseGeomeryPrimitives/Point3D.cs
@@ -3,37 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace BaseGeomeryPrimitives
 {
     public class Point3D
     {
-        readonly double[] _X;
+        readonly float[] _X;
 
-        public Point3D(double X, double Y, double Z)
+        public Point3D(float X, float Y, float Z)
         {
-            _X = new double[3];
+            _X = new float[3];
             _X[0] = X;
             _X[1] = Y;
             _X[2] = Z;
         }
 
-        public double X
+        public float X
         {
             get { return _X[0]; }
             set { _X[0] = value; }
         }
 
-        public double Y
+        public float Y
         {
             get { return _X[1]; }
             set { _X[1] = value; }
         }
 
-        public double Z
+        public float Z
         {
             get { return _X[2]; }
             set { _X[2] = value; }
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3(X, Y, Z);
+        }
+
+        public Vector4 ToVector4()
+        {
+            return new Vector4(X, Y, Z, 1.0f);
         }
     }
 }
---
+++ Advanced3DModels/BaseGeomeryPrimitives/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace BaseGeomeryPrimitives
+{
+    public class Triangle
+    {
+        public Point3D Point1 { get; set; }
+        public Point3D Point2 { get; set; }
+        public Point3D Point3 { get; set; }
+
+        public Vector3 Normal
+        {
+            get
+            {
+                Vector3 left = Point2.ToVector3() - Point1.ToVector3();
+                Vector3 right = Point3.ToVector3() - Point1.ToVector3();
+
+                return Vector3.Cross(left, right);
+            }
+        }
+    }
+}

35cebbf1d9312424abcf7b0b10631fe436999938
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-01-16 21:07:40 +0300

    Add test priject

---
+++ Advanced3DModels/UnitTestModels3D/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestModels3D")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestModels3D")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("750ebe02-b97e-4cf5-a2a5-9fe075b2e502")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Advanced3DModels/UnitTestModels3D/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models3DLib;
+using System.Numerics;
+
+namespace UnitTestModels3D
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Description("Проверяем корректность нахождения вектора нормали к треугольнику.")]
+        public void TestCross()
+        {
+            Point3D point1 = new Point3D(0, 0, 0);
+            Point3D point2 = new Point3D(1, 0, 0);
+            Point3D point3 = new Point3D(0, 2, 0);
+
+            Triangle triangle = new Triangle(point1, point2, point3);
+            Vector3 v = triangle.Normal;
+
+            Assert.IsTrue(v.X == 0 && v.Y == 0 && v.Z == 2);
+        }
+    }
+}

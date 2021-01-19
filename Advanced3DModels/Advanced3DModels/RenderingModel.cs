37c3cea6765a3d1b5c83a16924db8c3e4e72dd97
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-01-19 15:55:01 +0300

    Render edges

--- Advanced3DModels/Advanced3DModels/RenderingModel.cs
+++ Advanced3DModels/Advanced3DModels/RenderingModel.cs
@@ -8,11 +8,24 @@ using Models3DLib;
 
 namespace Advanced3DModels
 {
+    enum RenderType
+    {
+        Edges,
+        Triangulations,
+        Fill
+    }
+
     class RenderingModel
     {
-        public static void Render(Graphics g, Model model)
+        public static void Render(Graphics g, Model model, RenderType renderType)
         {
-            // TODO:
+            if (RenderType.Edges == renderType)
+            {
+                foreach(Edge edge in model.Edges)
+                {
+                    g.DrawLine(Pens.Black, edge.Point1.ToPointF(), edge.Point2.ToPointF());
+                }
+            }
         }
     }
 }

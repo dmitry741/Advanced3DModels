cca643276a41070aec21f3607fe1824645f4b9ed
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-01-19 15:21:48 +0300

    Begin develop class for rendering model

---
+++ Advanced3DModels/Advanced3DModels/RenderingModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Models3DLib;
+
+namespace Advanced3DModels
+{
+    class RenderingModel
+    {
+        public static void Render(Graphics g, Model model)
+        {
+            // TODO:
+        }
+    }
+}

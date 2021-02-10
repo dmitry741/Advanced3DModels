1720185cb35072e79f95111b6a4ed61540485dbb
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-02-10 17:38:43 +0300

    Add class Point3DColor

---
+++ Advanced3DModels/BaseGeomeryPrimitives/Point3DColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Models3DLib
+{
+    public class Point3DColor : Point3D
+    {
+        public Color ColorForRender { get; set; }
+
+        public Point3DColor(float X, float Y, float Z) : base(X, Y, Z)
+        {
+            ColorForRender = Color.Black;
+        }
+    }
+}

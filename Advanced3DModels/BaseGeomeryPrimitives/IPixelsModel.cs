579b0783cb5bfd0f40e56869aef3680b96744f27
dmitry741 <dmitrypavlov74@gmail.com>
2021-02-22 22:21:54 +0300

    Add public interface IPixelsModel

---
+++ Advanced3DModels/BaseGeomeryPrimitives/IPixelsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models3DLib
+{
+    public interface IPixelsModel
+    {
+        bool Contains(PointF point);
+        RectangleF BoundRect { get; }
+        Vector3 GetNormal(float X, float Y);
+    }
+}

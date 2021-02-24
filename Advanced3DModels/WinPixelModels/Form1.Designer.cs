d2d95a82e7278957686503067e24415a6f247d8e
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-02-24 17:44:42 +0300

    Begin dev GUI for pixel models

--- Advanced3DModels/WinPixelModels/Form1.Designer.cs
+++ Advanced3DModels/WinPixelModels/Form1.Designer.cs
@@ -28,22 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmbModels = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 560);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmbModels
+            // 
+            this.cmbModels.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModels.FormattingEnabled = true;
+            this.cmbModels.Location = new System.Drawing.Point(674, 12);
+            this.cmbModels.Name = "cmbModels";
+            this.cmbModels.Size = new System.Drawing.Size(121, 21);
+            this.cmbModels.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(622, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Модель";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 538);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(807, 581);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbModels);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Рендеринг моделей";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmbModels;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Advanced3DModels/WinPixelModels/Form1.cs
+++ Advanced3DModels/WinPixelModels/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Models3DLib;
 
 namespace WinPixelModels
 {
@@ -15,6 +16,60 @@ namespace WinPixelModels
         public Form1()
         {
             InitializeComponent();
+        }
+
+        #region === members ===
+
+        Bitmap _bitmap;
+        IPixelsModel _ipixelsModel;
+
+        #endregion
+
+        #region === private methods ===
+
+        void Render()
+        {
+            if (_bitmap == null || _ipixelsModel == null)
+                return;
+
+            // создание контекста для рисования
+            Graphics g = Graphics.FromImage(_bitmap);
+
+            // отрисовка фона
+            g.Clear(Color.White);
+
+            // TODO:
+
+            pictureBox1.Image = _bitmap;
+        }
+
+        IPoint3D ResolvePoint3D(float X, float Y, float Z)
+        {
+            return new Point3D(X, Y, Z);
+        }
+
+        IPixelsModel GetModel(int index)
+        {
+            IPoint3D point3D = ResolvePoint3D(pictureBox1.Width / 2, pictureBox1.Height / 2, 0);
+            float radius = Math.Min(pictureBox1.Width / 2, pictureBox1.Height / 2) - 80;
+
+            return new Sphere(point3D, radius, Color.DarkGray);
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+            _bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            _ipixelsModel = GetModel(0);
+
+            // TODO:
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }

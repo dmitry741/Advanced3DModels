2e0d9c9738eb5dd3ac28e1957201aa4d7477d287
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-03-03 11:13:56 +0300

    Create win app for texture models

---
+++ Advanced3DModels/WinTextureModels/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Models3DLib;
+
+namespace WinTextureModels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region === members ===
+
+        Bitmap _bitmap = null;
+        IEnumerable<ILightSource> _lightSources = null;
+        IPoint3D _pointObserverView = null;
+        bool _blockEvent = false;
+
+        #endregion
+
+        #region === private ===
+
+        void Render()
+        {
+            if (_bitmap == null)
+                return;
+
+            // создание контекста для рисования
+            Graphics g = Graphics.FromImage(_bitmap);
+
+            // отрисовка фона
+            g.Clear(Color.White);
+
+            // TODO:
+
+            pictureBox1.Image = _bitmap;
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+
+            _bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            // TODO
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+    }
+}

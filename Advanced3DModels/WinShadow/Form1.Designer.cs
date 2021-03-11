d4c966f564c8dc62bf6f3ef7d87172116048e314
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-03-11 16:58:21 +0300

    Add new sample WinShadow

---
+++ Advanced3DModels/WinShadow/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WinShadow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(724, 604);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(757, 555);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(107, 37);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Выход";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(826, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(754, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Модель";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 628);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Отображение теней";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Advanced3DModels/WinShadow/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Models3DLib;
+using System.Numerics;
+
+namespace WinShadow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region === members ===
+
+        Bitmap _bitmap;
+        Model _model;
+        IPoint3D _pointObserver = null;
+        ILightSource _lightSource = null;
+        IPerspectiveTransform _iperspectiveTransform = new PerspectiveTransformation();
+        PointF _startPoint;
+
+        bool _blockEvents = false;
+
+        #endregion
+
+        #region === private methods ===
+
+        Model GetModel(int index)
+        {
+            const float cSizePrimitive = 12;
+
+            Model model;
+
+            if (index == 0)
+            {
+                model = PresetsModel.Cube(120.0f, cSizePrimitive);
+            }
+            else
+            {
+                const float cScaleFactor = 0.5f;
+                model = PresetsModel.Arrow(200 * cScaleFactor, 60 * cScaleFactor, 60 * cScaleFactor, 100 * cScaleFactor, 24, cSizePrimitive, Color.DarkGoldenrod);
+            }
+
+            return model;
+        }
+
+        IPoint3D ResolvePoint3D(float X, float Y, float Z)
+        {
+            return new Point3D(X, Y, Z);
+        }
+
+        void RenderModel(Graphics g, Model model, ILightSource lightSource, IPoint3D pointObserver)
+        {
+            LightModelParameters lightModelParameters = new LightModelParameters
+            {
+                LightSources = new List<ILightSource> { lightSource },
+                PointObserver = pointObserver,
+            };
+
+            IEnumerable<Triangle> triangles = model.GetTrianglesForRender(RenderModelType.FillFull);
+            Color[] colors = new Color[3];
+
+            foreach (Triangle triangle in triangles)
+            {
+                lightModelParameters.Normal = triangle.Normal;
+                lightModelParameters.ReflectionEnable = triangle.ReflectionEnable;
+                lightModelParameters.ReflectionBrightness = triangle.ReflectionBrightness;
+                lightModelParameters.ReflcetionCone = triangle.ReflectionCone;
+                lightModelParameters.BaseColor = triangle.BaseColor;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    lightModelParameters.Point = triangle.Point3Ds[i];
+                    colors[i] = LightModel.GetColor(lightModelParameters);
+                }
+
+                int R = Convert.ToInt32(colors.Average(x => x.R));
+                int G = Convert.ToInt32(colors.Average(x => x.G));
+                int B = Convert.ToInt32(colors.Average(x => x.B));
+
+                Color color = Color.FromArgb(R, G, B);
+                Brush brush = new SolidBrush(color);
+
+                g.FillPolygon(brush, triangle.Points);
+            }
+        }
+
+        void Render()
+        {
+            if (_bitmap == null || _model == null)
+                return;
+
+            Graphics g = Graphics.FromImage(_bitmap);
+
+            // отрисовка фона
+            g.Clear(Color.White);
+
+            // запомнили состояние модели
+            _model.SaveState();
+
+            // перенос модели в центр окна
+            float xTranslate = pictureBox1.Width / 4;
+            float yTranslate = pictureBox1.Height / 2;
+            Matrix4x4 translate = Matrix4x4.CreateTranslation(xTranslate, yTranslate, 0f);
+            _model.Transform(translate);
+
+            // отрисовка модели
+            bool bPerspective = false;
+
+            if (bPerspective)
+            {
+                // перспективное преобразование
+                _model.Transform(_iperspectiveTransform, _pointObserver);
+            }
+
+            // отрисовка в главном окне
+            RenderModel(g, _model, _lightSource, _pointObserver);
+
+            // восстановили сохраненное состояние
+            _model.RestoreState();
+
+            pictureBox1.Image = _bitmap;
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+            _bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            _blockEvents = true;
+
+            comboBox1.BeginUpdate();
+            comboBox1.Items.AddRange(new string[] { "Куб", "Стрелка" });
+            comboBox1.SelectedIndex = 0;
+            comboBox1.EndUpdate();
+
+            _blockEvents = false;
+
+            _model = GetModel(0);
+
+            _lightSource = new ProjectorLightSource
+            {
+                VectorLightSource = new Vector3(-0.05f, -0.05f, -1.0f)
+            };
+
+            _pointObserver = ResolvePoint3D(pictureBox1.Width / 2, pictureBox1.Height / 2, -1400);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_blockEvents)
+                return;
+
+            _model = GetModel(comboBox1.SelectedIndex);
+            Render();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _startPoint = new PointF(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_model == null)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                const float cDivider = 100;
+
+                float angleXZ = -(e.X - _startPoint.X) / cDivider;
+                float angleYZ = (e.Y - _startPoint.Y) / cDivider;
+
+                Matrix4x4 matrixRotationXZ = Matrix4x4.CreateRotationY(angleXZ);
+                Matrix4x4 matrixRotationYZ = Matrix4x4.CreateRotationX(angleYZ);
+                Matrix4x4 matrix = matrixRotationXZ * matrixRotationYZ;
+
+                _model.Transform(matrix);
+
+                Render();
+
+                _startPoint.X = e.X;
+                _startPoint.Y = e.Y;
+            }
+        }
+    }
+}

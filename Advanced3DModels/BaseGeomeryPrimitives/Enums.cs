2f1c564e4cb381687f60c2bbd718ac6a4405c14b
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-03-22 17:55:38 +0300

    Begin to description of the main lib.

--- Advanced3DModels/BaseGeomeryPrimitives/Enums.cs
+++ Advanced3DModels/BaseGeomeryPrimitives/Enums.cs
@@ -6,17 +6,45 @@ using System.Threading.Tasks;
 
 namespace Models3DLib
 {
+    /// <summary>
+    /// Перечисление типов отрисовки модели.
+    /// </summary>
     public enum RenderModelType
     {
+        /// <summary>
+        /// Только трегуольники без закрашивания.
+        /// </summary>
         Triangulations,
+
+        /// <summary>
+        /// Полный рендеринг с освещением модели. 
+        /// </summary>
         FillFull,
+
+        /// <summary>
+        /// Рендеринг модели без освещения. Все треугольники закрашиваются одном цветом.
+        /// </summary>
         FillSolidColor
     }
 
+    /// <summary>
+    /// Перечисление типов освещения модели.
+    /// </summary>
     public enum RenderFillTriangle
     {
+        /// <summary>
+        /// Вычисления цвета треугольника по одной точке (вершине).
+        /// </summary>
         Flat0,
+
+        /// <summary>
+        /// Вычисления цвета треугольника по трем точкам (вершинам).
+        /// </summary>
         Flat3,
+
+        /// <summary>
+        /// Метод Гуро. Градиентная закраска треугольника.
+        /// </summary>
         Gouraud
     }
 }
--- Advanced3DModels/BaseGeomeryPrimitives/IFog.cs
+++ Advanced3DModels/BaseGeomeryPrimitives/IFog.cs
@@ -7,9 +7,22 @@ using System.Drawing;
 
 namespace Models3DLib
 {
+    /// <summary>
+    /// Интерфейс декларирующий методы для учета влияния тумана на рендеринг модели.
+    /// </summary>
     public interface IFog
     {
+        /// <summary>
+        /// Коррекция цвета с учетом тумана.
+        /// </summary>
+        /// <param name="z">Z координата.</param>
+        /// <param name="color">Цвет модели.</param>
+        /// <returns>Объект Color. Скорректированный с учетом тумана цвет.</returns>
         Color Correct(float z, Color color);
+
+        /// <summary>
+        /// Флаг необходимости коррекции цвета с учетом тумана.
+        /// </summary>
         bool Enabled { get; set; }
     }
 }
